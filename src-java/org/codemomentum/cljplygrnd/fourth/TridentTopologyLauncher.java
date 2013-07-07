b55990762730cabc14e564010f408ef93ec4ee21
Halit Olali <halitolali@gmail.com>
2013-07-07 09:35:31 +0300

    trident sample

--- src-java/org/codemomentum/cljplygrnd/fourth/TridentTopologyLauncher.java
+++ src-java/org/codemomentum/cljplygrnd/fourth/TridentTopologyLauncher.java
@@ -3,80 +3,121 @@ package org.codemomentum.cljplygrnd.fourth;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.LocalDRPC;
-import backtype.storm.StormSubmitter;
 import backtype.storm.generated.StormTopology;
+import backtype.storm.task.TopologyContext;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
-import org.codemomentum.cljplygrnd.first.RandomSentenceSpout;
-import storm.trident.TridentState;
 import storm.trident.TridentTopology;
 import storm.trident.operation.BaseFunction;
+import storm.trident.operation.Filter;
 import storm.trident.operation.TridentCollector;
-import storm.trident.operation.builtin.Count;
-import storm.trident.operation.builtin.FilterNull;
-import storm.trident.operation.builtin.MapGet;
+import storm.trident.operation.TridentOperationContext;
 import storm.trident.operation.builtin.Sum;
-import storm.trident.testing.MemoryMapState;
+import storm.trident.spout.IBatchSpout;
 import storm.trident.tuple.TridentTuple;
+
+import java.util.Map;
+import java.util.Random;
 
 
 public class TridentTopologyLauncher {
 
-    public static class Split extends BaseFunction {
+    public static class RandomNumberBatchSpout implements IBatchSpout {
+        Random random;
+
         @Override
-        public void execute(TridentTuple tuple, TridentCollector collector) {
-            String sentence = tuple.getString(0);
-            for(String word: sentence.split(" ")) {
-                collector.emit(new Values(word));
+        public void open(Map map, TopologyContext topologyContext) {
+            this.random = new Random();
+        }
+
+        @Override
+        public void emitBatch(long batchSize, TridentCollector tridentCollector) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            for (int i = 0; i < batchSize; i++) {
+                Values next = getNext();
+                System.out.println("** Generated : " + next);
+                tridentCollector.emit(next);
+            }
+        }
+
+        private Values getNext() {
+            return new Values(random.nextInt(100));
+        }
+
+        @Override
+        public void ack(long l) {
+
+        }
+
+        @Override
+        public void close() {
+
+        }
+
+        @Override
+        public Map getComponentConfiguration() {
+            return new Config();
+        }
+
+        @Override
+        public Fields getOutputFields() {
+            return new Fields("number");
         }
     }
 
-    public static StormTopology buildTopology(LocalDRPC drpc) {
+    public static class TentimesFunction extends BaseFunction {
+        @Override
+        public void execute(TridentTuple tuple, TridentCollector collector) {
+            Integer number = tuple.getInteger(0);
+            collector.emit(new Values(number * 10));
+            System.out.println("** Processed " + (number * 10));
+        }
+    }
 
+    public static class PrintFilter implements Filter {
 
+        @Override
+        public void prepare(Map conf, TridentOperationContext context) {
+        }
+
+        @Override
+        public void cleanup() {
+        }
+
+        @Override
+        public boolean isKeep(TridentTuple tuple) {
+            System.out.println("***" + tuple);
+            return true;
+        }
+    }
+
+    public static StormTopology buildTopology() {
         TridentTopology topology = new TridentTopology();
-        TridentState wordCounts =
-                topology.newStream("spout1", new RandomSentenceSpout())
-                        .parallelismHint(16)
-                        .each(new Fields("sentence"), new Split(), new Fields("word"))
-                        //stream is grouped by the "word" field.
-                        .groupBy(new Fields("word"))
-                        //each group is persistently aggregated using the Count aggregator
-                        .persistentAggregate(new MemoryMapState.Factory(),     //the word counts are kept in memory,
-                                                                               // but this can be trivially swapped to use Memcached, Cassandra, or any other persistent store
-                                new Count(), new Fields("count"))
-                        .parallelismHint(16);
+        topology.newStream("RandomNumberSpout", new RandomNumberBatchSpout())
+                .parallelismHint(16)
+                .each(new Fields("number"), new TentimesFunction(), new Fields("double"))
+                .partitionAggregate(new Fields("double"), new Sum(), new Fields("sum"))
+                .each(new Fields("sum"), new PrintFilter());
         //the values stored by persistentAggregate represents the aggregation of all batches ever emitted by the stream.
         //The persistentAggregate method transforms a Stream into a TridentState object. In this case the TridentState object represents all the word counts.
         // We will use this TridentState object to implement the distributed query portion of the computation.
 
-
-        topology.newDRPCStream("words", drpc)
-                .each(new Fields("args"), new Split(), new Fields("word"))
-                .groupBy(new Fields("word"))
-                .stateQuery(wordCounts, new Fields("word"), new MapGet(), new Fields("count"))
-                .each(new Fields("count"), new FilterNull())
-                .aggregate(new Fields("count"), new Sum(), new Fields("sum"))
-        ;
         return topology.build();
     }
+
 
     public static void main(String[] args) throws Exception {
         Config conf = new Config();
         conf.setMaxSpoutPending(20);
         conf.setNumWorkers(1);
-        if(args.length==0) {
-            LocalDRPC drpc = new LocalDRPC();
-            LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology("wordCounter", conf, buildTopology(drpc));
-            for(int i=0; i<100; i++) {
-                System.out.println("DRPC RESULT: " + drpc.execute("words", "cat the dog jumped"));
-                Thread.sleep(1000);
-            }
-        } else {
-            conf.setNumWorkers(3);
-            StormSubmitter.submitTopology(args[0], conf, buildTopology(null));
-        }
+
+        LocalDRPC drpc = new LocalDRPC();
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("numbertopology", conf, buildTopology());
+
     }
 }
